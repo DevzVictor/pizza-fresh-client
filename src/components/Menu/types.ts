2c5156dd3f1cc4e6166853d95bfe608320837f84
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-17 12:28:48 -0300

    Preparando arquitetura base do projeto

---
+++ src/components/Menu/types.ts
@@ -0,0 +1,6 @@
+import { RoutePath } from "types/routes";
+
+export interface NavItem {
+  con: JSX.Element;
+  path: RoutePath;
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import Menu from "components/Menu";
+import * as S from "./style";
+
+const Home = () => {
+  return (
+    <S.Home>
+      <Menu />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficara a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor"></input>
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>detalhes do pedido</p>
+      </aside>
+    </S.Home>
+  );
+};
+
+export default Home;
