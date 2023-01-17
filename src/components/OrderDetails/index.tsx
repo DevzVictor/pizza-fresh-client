f01c8107ecf8ec31f743b10c55e09edb0efe5775
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-17 14:30:40 -0300

    feat(orderdetail): create and add 'order item list '

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,22 +1,40 @@
 import * as S from "./style";
 import ButtonToggle from "components/ButtonToggle";
+import OrderItemList from "components/OrderItemList";
+import ButtonLarge from "components/ButtonLarge";
 
 const OrderDetails = () => {
   return (
     <S.OrderDetails>
-        <S.OrderDetailsTitle>
-            Detalhes do Pedido
-        </S.OrderDetailsTitle>
-        <S.OrderDetailsButtonGroup>
-            <ButtonToggle active={true} value="Comer no lugar" />
-            <ButtonToggle active={false} value="P/ Viagem" />
-            <ButtonToggle active={true} value="Delivery" />
-        </S.OrderDetailsButtonGroup>
-        <S.OrderDetailsList>
-            <p>Itens do pedido</p>
-        </S.OrderDetailsList>
+      <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={true} value="Comer no lugar" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={true} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
+      <S.OrderDetailsList>
+        <OrderItemList
+          header={
+            <S.OrderDetailsListTitle>
+              <h4>Item</h4>
+              <h4>Qtd</h4>
+              <h4>Preço</h4>
+            </S.OrderDetailsListTitle>
+          }
+          list={"list de itens"}
+          footer={
+            <S.OrderDetailsListFooter>
+              <S.OrderDetailsListFooterRow>
+                <span>SubTotal</span>
+                <span>R$ 10.00</span>
+              </S.OrderDetailsListFooterRow>
+              <ButtonLarge value="Continue para pagamento"></ButtonLarge>
+            </S.OrderDetailsListFooter>
+          }
+        />
+      </S.OrderDetailsList>
     </S.OrderDetails>
   );
-}
+};
 
-export default OrderDetails
+export default OrderDetails;
