846330961eb40109e628b10708c35ee0cda63c4d
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-19 10:28:53 -0300

    feat: implements product functions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,11 +7,13 @@ import OrderDetails from "components/OrderDetails";
 import Overlay from "components/Overlay";
 import CheckoutSection from "components/CheckoutSection";
 import { navigationItems } from "data/navigation";
-import { products } from "mocks/products";
 import { ProductResponse } from "types/Product";
 import { OrderType } from "types/orderType";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { OrderItemType } from "types/OrderItemType";
+import { useQuery } from "react-query";
+import { QueryKey } from "types/QueryKey";
+import { ProductService } from "services/ProductService";
 
 import { DateTime } from "luxon";
 
@@ -26,6 +28,13 @@ const Home = () => {
   });
 
   const navigate = useNavigate();
+
+  const { data: productsData } = useQuery(
+    [QueryKey.PRODUCTS],
+    ProductService.getLista
+  );
+
+  const [products, setProducts] = useState<ProductResponse[]>([]);
 
   const [activeOrderType, setActiverOrderType] = useState(
     OrderType.COMER_NO_LOCAL
@@ -54,6 +63,10 @@ const Home = () => {
     const filtered = orders.filter((i) => i.product.id !== id);
     setOrders(filtered);
   };
+
+  useEffect(() => {
+    setProducts(productsData || []);
+  }, [productsData]);
 
   return (
     <S.Home>
